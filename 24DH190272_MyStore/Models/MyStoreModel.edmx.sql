e098b9180f36fe4a9d6675fd10f2ef38c0d158bc
DatPham06 <pdat1969@gmail.com>
2025-11-20 20:54:21 +0700

    responsive va ProductDetail

---
+++ 24DH190272_MyStore/Models/MyStoreModel.edmx.sql
@@ -0,0 +1,264 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/18/2025 21:17:27
+-- Generated from EDMX file: C:\Users\thanh\OneDrive\Documents\WebProject1\MyStore\24DH190272_MyStore\Models\MyStoreModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MyStore];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK__Order__CustomerI__412EB0B6]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Order] DROP CONSTRAINT [FK__Order__CustomerI__412EB0B6];
+GO
+IF OBJECT_ID(N'[dbo].[FK__OrderDeta__Order__44FF419A]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[OrderDetail] DROP CONSTRAINT [FK__OrderDeta__Order__44FF419A];
+GO
+IF OBJECT_ID(N'[dbo].[FK__OrderDeta__Produ__440B1D61]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[OrderDetail] DROP CONSTRAINT [FK__OrderDeta__Produ__440B1D61];
+GO
+IF OBJECT_ID(N'[dbo].[FK_Pro_Category]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Product] DROP CONSTRAINT [FK_Pro_Category];
+GO
+IF OBJECT_ID(N'[dbo].[FK_User_Customer]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Customer] DROP CONSTRAINT [FK_User_Customer];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Category]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Category];
+GO
+IF OBJECT_ID(N'[dbo].[Customer]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Customer];
+GO
+IF OBJECT_ID(N'[dbo].[Order]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Order];
+GO
+IF OBJECT_ID(N'[dbo].[OrderDetail]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[OrderDetail];
+GO
+IF OBJECT_ID(N'[dbo].[Product]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Product];
+GO
+IF OBJECT_ID(N'[dbo].[sysdiagrams]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[sysdiagrams];
+GO
+IF OBJECT_ID(N'[dbo].[User]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[User];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Categories'
+CREATE TABLE [dbo].[Categories] (
+    [CategoryID] int IDENTITY(1,1) NOT NULL,
+    [CategoryName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Customers'
+CREATE TABLE [dbo].[Customers] (
+    [CustomerID] int IDENTITY(1,1) NOT NULL,
+    [CustomerName] nvarchar(max)  NOT NULL,
+    [CustomerPhone] nvarchar(15)  NOT NULL,
+    [CustomerEmail] nvarchar(max)  NULL,
+    [CustomerAddress] nvarchar(max)  NULL,
+    [Username] nvarchar(255)  NOT NULL
+);
+GO
+
+-- Creating table 'Orders'
+CREATE TABLE [dbo].[Orders] (
+    [OrderID] int IDENTITY(1,1) NOT NULL,
+    [CustomerID] int  NOT NULL,
+    [OrderDate] datetime  NOT NULL,
+    [TotalAmount] decimal(18,2)  NOT NULL,
+    [PaymentStatus] nvarchar(max)  NULL,
+    [ShippingAddress] nvarchar(max)  NOT NULL,
+    [DeliveryMethod] nvarchar(50)  NULL,
+    [PaymentMethod] nvarchar(50)  NULL
+);
+GO
+
+-- Creating table 'OrderDetails'
+CREATE TABLE [dbo].[OrderDetails] (
+    [ID] int IDENTITY(1,1) NOT NULL,
+    [ProductID] int  NOT NULL,
+    [OrderID] int  NOT NULL,
+    [Quantity] int  NOT NULL,
+    [UnitPrice] decimal(18,2)  NOT NULL,
+    [TotalPrice] decimal(29,2)  NULL
+);
+GO
+
+-- Creating table 'Products'
+CREATE TABLE [dbo].[Products] (
+    [ProductID] int IDENTITY(1,1) NOT NULL,
+    [CategoryID] int  NOT NULL,
+    [ProductName] nvarchar(max)  NOT NULL,
+    [ProductPrice] decimal(18,2)  NOT NULL,
+    [ProductImage] nvarchar(max)  NULL,
+    [ProductDescription] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'sysdiagrams'
+CREATE TABLE [dbo].[sysdiagrams] (
+    [name] nvarchar(128)  NOT NULL,
+    [principal_id] int  NOT NULL,
+    [diagram_id] int IDENTITY(1,1) NOT NULL,
+    [version] int  NULL,
+    [definition] varbinary(max)  NULL
+);
+GO
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Username] nvarchar(255)  NOT NULL,
+    [Password] nchar(50)  NOT NULL,
+    [UserRole] nchar(1)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [CategoryID] in table 'Categories'
+ALTER TABLE [dbo].[Categories]
+ADD CONSTRAINT [PK_Categories]
+    PRIMARY KEY CLUSTERED ([CategoryID] ASC);
+GO
+
+-- Creating primary key on [CustomerID] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [PK_Customers]
+    PRIMARY KEY CLUSTERED ([CustomerID] ASC);
+GO
+
+-- Creating primary key on [OrderID] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [PK_Orders]
+    PRIMARY KEY CLUSTERED ([OrderID] ASC);
+GO
+
+-- Creating primary key on [ID] in table 'OrderDetails'
+ALTER TABLE [dbo].[OrderDetails]
+ADD CONSTRAINT [PK_OrderDetails]
+    PRIMARY KEY CLUSTERED ([ID] ASC);
+GO
+
+-- Creating primary key on [ProductID] in table 'Products'
+ALTER TABLE [dbo].[Products]
+ADD CONSTRAINT [PK_Products]
+    PRIMARY KEY CLUSTERED ([ProductID] ASC);
+GO
+
+-- Creating primary key on [diagram_id] in table 'sysdiagrams'
+ALTER TABLE [dbo].[sysdiagrams]
+ADD CONSTRAINT [PK_sysdiagrams]
+    PRIMARY KEY CLUSTERED ([diagram_id] ASC);
+GO
+
+-- Creating primary key on [Username] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Username] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [CategoryID] in table 'Products'
+ALTER TABLE [dbo].[Products]
+ADD CONSTRAINT [FK_Pro_Category]
+    FOREIGN KEY ([CategoryID])
+    REFERENCES [dbo].[Categories]
+        ([CategoryID])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_Pro_Category'
+CREATE INDEX [IX_FK_Pro_Category]
+ON [dbo].[Products]
+    ([CategoryID]);
+GO
+
+-- Creating foreign key on [CustomerID] in table 'Orders'
+ALTER TABLE [dbo].[Orders]
+ADD CONSTRAINT [FK__Order__CustomerI__412EB0B6]
+    FOREIGN KEY ([CustomerID])
+    REFERENCES [dbo].[Customers]
+        ([CustomerID])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK__Order__CustomerI__412EB0B6'
+CREATE INDEX [IX_FK__Order__CustomerI__412EB0B6]
+ON [dbo].[Orders]
+    ([CustomerID]);
+GO
+
+-- Creating foreign key on [Username] in table 'Customers'
+ALTER TABLE [dbo].[Customers]
+ADD CONSTRAINT [FK_User_Customer]
+    FOREIGN KEY ([Username])
+    REFERENCES [dbo].[Users]
+        ([Username])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_User_Customer'
+CREATE INDEX [IX_FK_User_Customer]
+ON [dbo].[Customers]
+    ([Username]);
+GO
+
+-- Creating foreign key on [OrderID] in table 'OrderDetails'
+ALTER TABLE [dbo].[OrderDetails]
+ADD CONSTRAINT [FK__OrderDeta__Order__44FF419A]
+    FOREIGN KEY ([OrderID])
+    REFERENCES [dbo].[Orders]
+        ([OrderID])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK__OrderDeta__Order__44FF419A'
+CREATE INDEX [IX_FK__OrderDeta__Order__44FF419A]
+ON [dbo].[OrderDetails]
+    ([OrderID]);
+GO
+
+-- Creating foreign key on [ProductID] in table 'OrderDetails'
+ALTER TABLE [dbo].[OrderDetails]
+ADD CONSTRAINT [FK__OrderDeta__Produ__440B1D61]
+    FOREIGN KEY ([ProductID])
+    REFERENCES [dbo].[Products]
+        ([ProductID])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK__OrderDeta__Produ__440B1D61'
+CREATE INDEX [IX_FK__OrderDeta__Produ__440B1D61]
+ON [dbo].[OrderDetails]
+    ([ProductID]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
